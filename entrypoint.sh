8ad92df1e73fc9906b3a3b42a0d36e3606d07f2f
Mack Naive <mack@bitnaive.com>
2018-06-20 11:17:37 -0400

    bad new lines in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 cp /Linda.conf.template /root/.Linda/Linda.conf
-echo "\n masternodeprivkey=$MASTERNODEPRIVKEY" >> /root/.Linda/Linda.conf
-echo "\n port=$PORT" >> /root/.Linda/Linda.conf
-echo "\n rpcuser=$RPCUSER" >> /root/.Linda/Linda.conf
-echo "\n rpcpassword=$RPCPASSWORD">> /root/.Linda/Linda.conf
+echo "masternodeprivkey=$MASTERNODEPRIVKEY" >> /root/.Linda/Linda.conf
+echo "port=$PORT" >> /root/.Linda/Linda.conf
+echo "rpcuser=$RPCUSER" >> /root/.Linda/Linda.conf
+echo "rpcpassword=$RPCPASSWORD">> /root/.Linda/Linda.conf
 
 /Lindad -printtoconsole & tail -f /dev/null
 
